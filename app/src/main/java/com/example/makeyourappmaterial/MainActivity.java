e886739f6a89d997b7cd141ae8b3b3455bb16fe7
Gahee Han <leisure306@gmail.com>
2019-06-18 14:43:02 +0900

    feat : Implement custom Floating action button Compare it with the material library FAB by displaying a Toast

--- app/src/main/java/com/example/makeyourappmaterial/MainActivity.java
+++ app/src/main/java/com/example/makeyourappmaterial/MainActivity.java
@@ -3,12 +3,43 @@ package com.example.makeyourappmaterial;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageButton;
+import android.widget.Toast;
+
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
 
 public class MainActivity extends AppCompatActivity {
+
+
+    FloatingActionButton floatingActionButton;
+    ImageButton imageButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        floatingActionButton = findViewById(R.id.fab);
+        imageButton = findViewById(R.id.custom_fab);
+
+        setClickListenerToFabs();
+
+    }
+
+    private void setClickListenerToFabs(){
+        floatingActionButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Toast.makeText(MainActivity.this, "FAB from the material library.", Toast.LENGTH_SHORT).show();
+            }
+        });
+
+        imageButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Toast.makeText(MainActivity.this, "This is a custom fab.", Toast.LENGTH_SHORT).show();
+            }
+        });
     }
 }
